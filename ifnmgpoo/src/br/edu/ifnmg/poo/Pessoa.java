8e8c6e07f80e71c17a3c21d853a3e741b732291e
ifnmg <ifnmg@ifnmg-HP-EliteDesk-705-G1-SFF>
2018-05-04 14:59:58 -0300

    Criação de atributos e metodos iniciais

---
+++ ifnmgpoo/src/br/edu/ifnmg/poo/Pessoa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.poo;
+
+/**
+ *
+ * @author ifnmg
+ */
+public class Pessoa {
+    
+    private String nome;
+    
+    
+    private void setNome (String nome){
+        this.nome= nome;
+
+    }
+    
+    public String getNome(){
+         return this.nome;
+     }
+    
+}
