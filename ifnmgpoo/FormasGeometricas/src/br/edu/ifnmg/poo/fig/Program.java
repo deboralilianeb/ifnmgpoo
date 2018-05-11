752f82b5d3c177d957de4ae8e32db348d1df31ec
ifnmg <ifnmg@ifnmg-HP-EliteDesk-705-G1-SFF>
2018-05-11 14:19:44 -0300

    teste

---
+++ ifnmgpoo/FormasGeometricas/src/br/edu/ifnmg/poo/fig/Program.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.poo.fig;
+
+/**
+ *
+ * @author ifnmg
+ */
+public class Program {
+    
+    public static void main (String[] args){
+    
+            FiguraGeometrica fg1 = new FiguraGeometrica();      
+            
+    } 
+}   
